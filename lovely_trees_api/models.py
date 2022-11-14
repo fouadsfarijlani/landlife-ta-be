27a84923491939db57a5846e27e91d092014ad3b
Fouad Sfarijlani <fouad@Admins-MacBook-Pro.local>
2022-11-14 20:51:55 +0100

    Added forgein key to FieldData refercing Species

--- lovely_trees_api/models.py
+++ lovely_trees_api/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+class Species(models.Model):
+    tree_species_id = models.IntegerField(primary_key=True)
+    latin_name = models.CharField(max_length=100)
+
 class FieldData(models.Model):
     class HealthChoices(models.IntegerChoices):
         Poor = 1
@@ -7,12 +11,8 @@ class FieldData(models.Model):
         Excellent = 3
         NA = 4
     individual_tree_id = models.IntegerField(primary_key=True)
-    species_id = models.IntegerField()
+    species_id = models.ForeignKey(Species, on_delete=models.CASCADE)
     method = models.CharField(max_length=50)
     height = models.IntegerField()
     health = models.IntegerField(choices=HealthChoices.choices)
     year_monitored = models.CharField(max_length=4)
-
-class Species(models.Model):
-    tree_species_id = models.IntegerField(primary_key=True)
-    latin_name = models.CharField(max_length=100)
