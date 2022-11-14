7917fc9f9ae8e587614423f22fad06326627ffae
Fouad Sfarijlani <fouad@Admins-MacBook-Pro.local>
2022-11-14 20:15:52 +0100

    Created first draft of FieldData model

--- lovely_trees_api/models.py
+++ lovely_trees_api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class FieldData(models.Model):
+    class HealthChoices(models.Model):
+        Poor = 1
+        Ok = 2
+        Excellent = 3
+        NA = 4
+    individual_tree_id = models.IntegerField(primary_key=True)
+    species_id = models.IntegerField()
+    method = models.CharField(max_length=50)
+    height = models.IntegerField()
+    health = models.IntegerChoices(choices = HealthChoices)
+    year_monitored = models.CharField(max_length=4)
